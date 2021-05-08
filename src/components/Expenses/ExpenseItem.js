c87c8c71eb74828fb0fbb8aec1daddbeef8dc264
Tobiasz <tobiasz0889@gmail.com>
2021-05-08 18:05:05 +0100

    Add expenses compononets and Card componenet

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+import ExpebnseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+  const { title, amount, date } = props;
+
+  return (
+    <Card className="expense-item">
+      <ExpebnseDate date={date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;

09671c0283fcd6964dcd9f4c5ecc5c1e8c3f2552
Tobiasz <tobiasz0889@gmail.com>
2021-05-22 13:49:20 +0100

    Added expenses filter and new expesne form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,23 @@
 import ExpebnseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
+import { useState } from 'react';
 
 const ExpenseItem = (props) => {
-  const { title, amount, date } = props;
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Updated!');
+  };
 
   return (
     <Card className="expense-item">
-      <ExpebnseDate date={date} />
+      <ExpebnseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
